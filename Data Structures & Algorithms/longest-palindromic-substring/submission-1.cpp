767cff460311364981398567a6e52258b5872741
Ashish Kumar <langyanashish@gmail.com>
2026-05-14 19:42:01 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resIdx = 0, resLen = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++)
+        {
+            //odd length
+            int low = i, high = i;
+            while(low >= 0 && high < n)
+            {
+                if(s[low] != s[high])
+                    break;
+                if((high-low+1) > resLen)
+                {
+                    resLen = high-low+1;
+                    resIdx = low;
+                }
+                low--;
+                high++;
+            }
+            //even length
+            low = i, high = i+1;
+            while(low >= 0 && high < n)
+            {
+                if(s[low] != s[high])
+                    break;
+                if((high-low+1) > resLen)
+                {
+                    resLen = high-low+1;
+                    resIdx = low;
+                }
+                low--;
+                high++;
+            }
+        }
+        return s.substr(resIdx, resLen);
+    }
+};
